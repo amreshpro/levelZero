11fe85e88d61528b33d37f9faac465646e0c6a88
amreshpro <amreshprof999@gmail.com>
2024-09-11 17:45:07 +0530

    add A2.3  and working in A2 exercise

---
+++ A2_Exercise_1.js
@@ -0,0 +1,27 @@
+console.log("A2_Exercise_1");
+console.log("---- ---- ---- ");
+console.log("Indian Bookstore Inventory Report");
+console.log("-------------------------------------");
+
+console.log("Book 1: Mahabharta");
+console.log("- Quantity: ");
+console.log("- Price: ");
+console.log("- Value: ");
+
+console.log("\n \n");
+
+console.log("Book 2: Ramayana");
+console.log("- Quantity: ");
+console.log("- Price: ");
+console.log("- Value: ");
+
+console.log("\n \n");
+
+console.log("Book 3: Gitanjali");
+console.log("- Quantity: ");
+console.log("- Price: ");
+console.log("- Value: ");
+
+console.log("\n \n");
+
+console.log("Total Inventory Value: ");

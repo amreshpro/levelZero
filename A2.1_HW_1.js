af0e02a1c2f52fc371b83cb548c318ccc0ed7cce
amreshpro <amreshprof999@gmail.com>
2024-09-11 10:03:43 +0530

    "start working on A2"

---
+++ A2.1_HW_1.js
@@ -0,0 +1,16 @@
+console.log("A2.1_HW_1")
+console.log("---- ---- ----")
+console.log("Exercise 1")
+console.log("---- ---- ----")
+
+console.log("1.1")
+
+let numOne = 8
+let numTwo = 6
+
+console.log("Numbers: " + numOne + " " + numTwo)
+console.log("Sum: ", numOne + numTwo)
+
+console.log("---- ---- ----")
+
+console.log("1.2")

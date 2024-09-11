7c3ec119b76fb3de893d9373d775fa8175d56294
amreshpro <amreshprof999@gmail.com>
2024-09-11 18:30:55 +0530

    A2 exercise done

---
+++ A2_Exercise_2.js
@@ -0,0 +1,82 @@
+console.log("A2_Exercise_2");
+console.log("---- ---- ----");
+console.log("Payment Chart:");
+
+let contractor1 = "Rajesh"
+let contractor2 = "Priya"
+let contractor3 = "Amit"
+let contractor4 = "Ananya"
+
+let jobDescription1 = "Web Developer"
+let jobDescription2 = "Content Writer"
+let jobDescription3 = "Graphic Designer"
+let jobDescription4 = "Digital Marketer"
+
+let hourlyRate1 = 300
+let hourlyRate2 = 250
+let hourlyRate3 = 350
+let hourlyRate4 = 200
+
+let hoursWorked1 = 80
+let hoursWorked2 = 60
+let hoursWorked3 = 40
+let hoursWorked4 = 20
+
+let ratings1 = 90
+let ratings2 = 80
+let ratings3 = 92
+let ratings4 = 88
+
+let payment1 = hourlyRate1 * hoursWorked1
+let payment2 = hourlyRate2 * hoursWorked2
+let payment3 = hourlyRate3 * hoursWorked3
+let payment4 = hourlyRate4 * hoursWorked4
+
+
+let isEligibleForGoodieBag1 = hoursWorked1 > 15 || ratings1 > 85
+let isEligibleForGoodieBag2 = hoursWorked2 > 15 || ratings2 > 85
+let isEligibleForGoodieBag3 = hoursWorked3 > 15 || ratings3 > 85
+let isEligibleForGoodieBag4 = hoursWorked4 > 15 || ratings4 > 85
+
+
+
+console.log("-------------------------");
+console.log("Contractor:",contractor1);
+console.log("Job Description: ",jobDescription1);
+console.log("Hourly Rate: ",hourlyRate1);
+console.log("Hours Worked: ",hoursWorked1);
+console.log("Total Payment: ",payment1);
+console.log("Rating: ",ratings1);
+console.log("Is Eligible for Goodie Bag: ",isEligibleForGoodieBag1);
+
+
+console.log("-------------------------");
+console.log("Contractor:",contractor2);
+console.log("Job Description: ",jobDescription2);
+console.log("Hourly Rate: ",hourlyRate2);
+console.log("Hours Worked: ",hoursWorked2);
+console.log("Total Payment: ",payment2);
+console.log("Rating: ",ratings2);
+console.log("Is Eligible for Goodie Bag: ",isEligibleForGoodieBag2);
+
+console.log("-------------------------");
+console.log("Contractor:",contractor3);
+console.log("Job Description: ",jobDescription3);
+console.log("Hourly Rate: ",hourlyRate3);
+console.log("Hours Worked: ",hoursWorked3);
+console.log("Total Payment: ",payment3);
+console.log("Rating: ",ratings3);
+console.log("Is Eligible for Goodie Bag: ",isEligibleForGoodieBag3);
+
+
+
+console.log("-------------------------");
+console.log("Contractor:",contractor4);
+console.log("Job Description: ",jobDescription4);
+console.log("Hourly Rate: ",hourlyRate4);
+console.log("Hours Worked: ",hoursWorked4);
+console.log("Total Payment: ",payment4);
+console.log("Rating: ",ratings4);
+console.log("Is Eligible for Goodie Bag: ",isEligibleForGoodieBag4);
+
+

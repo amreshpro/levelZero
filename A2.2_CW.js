64886c03af9b23b706a0e2766114bac233c72b1b
amreshpro <amreshprof999@gmail.com>
2024-09-11 12:09:13 +0530

    "workling on A2.2_CW"

---
+++ A2.2_CW.js
@@ -0,0 +1,33 @@
+console.log("A2.2_CW")
+console.log("---- ---- ----")
+console.log("Exercise 1")
+
+console.log("---- ---- ----")
+
+console.log("1.1")
+let rohitAge = 22
+let rahulAge = 23
+
+let isRohitOlder = rohitAge>rahulAge
+
+console.log("Is Rohit older than Rahul? ", isRohitOlder)
+
+console.log("---- ---- ----")
+
+console.log("1.2")
+let isRohitYounger = rohitAge<rahulAge
+
+console.log("Is Rohit younger than Rahul? ", isRohitYounger)
+
+
+console.log("---- ---- ----")
+
+console.log("1.3")
+let romaAge = 22
+
+let isRohitEqualRoma = rohitAge === romaAge
+console.log("Are Rohit and Roma of the same age? ",isRohitEqualRoma)
+
+console.log("---- ---- ----")
+
+console.log("1.4")

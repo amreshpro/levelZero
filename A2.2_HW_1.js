7bf751c6dedb4a9e371d24e4454a1754f4b2b644
amreshpro <amreshprof999@gmail.com>
2024-09-11 15:08:48 +0530

    working on A2.2

--- A2.2_HW_1.js
+++ A2.2_HW_1.js
@@ -4,7 +4,43 @@ console.log("Exercise 1")
 
 console.log("---- ---- ----")
 console.log("1.1")
-let akshayTime=10.75
-let raviTime=10.92
+let akshayTime = 10.75
+let raviTime = 10.92
+let isAkshayFaster = akshayTime < raviTime
+console.log("Is Akshay faster than Ravi in the 100m sprint?", isAkshayFaster)
+
+console.log("---- ---- ----")
+
+console.log("1.2")
+let akshayTime400 = 46.21
+let raviTime400 = 47.05
+let isAkshayFaster400 = akshayTime400 < raviTime400
+console.log("Is Akshay faster than Ravi in the 400m race?", isAkshayFaster400)
+
+console.log("---- ---- ----")
+
+console.log("1.3")
+let akshayDistance = 7.83
+let raviDistance = 7.68 
+let isAkshayLongerJump = akshayDistance>raviDistance
+console.log("Did Akshay make a longer jump than Ravi?", isAkshayLongerJump)
+
+console.log("---- ---- ----")
+
+console.log("1.4")
+let akshayDistanceShotPut = 19.75
+let raviDistanceShotPut = 18.92
+let isAkshayLongerThrow = akshayDistanceShotPut>raviDistanceShotPut
+console.log("Did Akshay make a longer throw than Ravi?", isAkshayLongerThrow)
+
+console.log("---- ---- ----")
+
+console.log("1.5")
+let akshayHeight = 2.05
+let raviHeight = 1.98
+let
+
+
+
 
 

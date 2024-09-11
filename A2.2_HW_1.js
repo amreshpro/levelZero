76412260778222f28b3c84b05be8d4c00e360ba8
amreshpro <amreshprof999@gmail.com>
2024-09-11 14:07:57 +0530

    A2.2_CW done

---
+++ A2.2_HW_1.js
@@ -0,0 +1,10 @@
+console.log("A2.2_HW_1")
+console.log("---- ---- ----")
+console.log("Exercise 1")
+
+console.log("---- ---- ----")
+console.log("1.1")
+let akshayTime=10.75
+let raviTime=10.92
+
+

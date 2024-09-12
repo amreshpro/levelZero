42d8a364f9eab77f779a1aa292281b36290c9bde
Amresh Maurya <amreshprof999@gmail.com>
2024-09-12 13:20:04 +0530

    add A2.1HW_1

---
+++ A2.1_HW_1.js
@@ -0,0 +1,94 @@
+console.log("A2.1_HW_1")
+console.log("---- ---- ----")
+console.log("Exercise 1")
+console.log("---- ---- ----")
+
+
+let numOne = 8
+let numTwo = 6
+
+console.log("1.1")
+let sum = numOne + numTwo
+console.log("Numbers: " + numOne + " " + numTwo)
+
+console.log("Sum: ", sum)
+
+console.log("---- ---- ----")
+
+console.log("1.2")
+
+let difference = numOne - numTwo
+
+console.log("Numbers: " + numOne + " " + numTwo)
+console.log("Difference: ", difference)
+
+console.log("---- ---- ----")
+
+console.log("1.3")
+
+let product = numOne * numTwo
+console.log("Numbers: " + numOne + " " + numTwo);
+console.log("Product: ", product);
+
+console.log("---- ---- ----")
+
+console.log("1.4")
+
+let division = numOne / numTwo
+console.log("Numbers: " + numOne + " " + numTwo);
+console.log("Divion: ", division);
+
+
+console.log("---- ---- ----")
+
+console.log("1.5")
+
+let modulo = numOne % numTwo
+console.log("Numbers: " + numOne + " " + numTwo);
+console.log("Remainder: ", modulo);
+
+console.log("---- ---- ----")
+console.log("Exercise 2")
+
+console.log("---- ---- ----")
+let numThree = 16
+
+console.log("2.1")
+let Sum = numOne + numTwo + numThree
+console.log("Numbers: " + numOne + " " + numTwo + " " + numThree)
+console.log("Sum All: ", Sum)
+
+console.log("---- ---- ----")
+
+console.log("2.2")
+
+let addAndSubstract = numThree - (numOne + numTwo)
+console.log("Numbers: " + numOne + " " + numTwo + " " + numThree)
+console.log("Add and Substract: ", addAndSubstract)
+
+console.log("---- ---- ----")
+
+console.log("2.3")
+
+let productAndAdd = (numOne * numTwo) + numThree
+console.log("Numbers: " + numOne + " " + numTwo + " " + numThree)
+console.log("Product and Add: ", productAndAdd)
+
+console.log("---- ---- ----")
+
+console.log("2.4")
+
+let divideAndAdd = (numThree / numOne) + numTwo
+console.log("Numbers: " + numOne + " " + numTwo + " " + numThree)
+console.log("Divide and Add: ", divideAndAdd)
+
+console.log("---- ---- ----")
+
+
+console.log("2.5")
+
+let average = (numOne + numTwo + numThree) / 3
+console.log("Numbers: " + numOne + " " + numTwo + " " + numThree)
+console.log("Average: ", average)
+
+console.log("---- ---- ----")

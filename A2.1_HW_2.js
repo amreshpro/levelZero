625564df0ee9c90e99f5d2c7d2e1814ac1400153
amreshpro <amreshprof999@gmail.com>
2024-09-12 15:10:57 +0530

    A2 done

--- A2.1_HW_2.js
+++ A2.1_HW_2.js
@@ -9,8 +9,8 @@ let numTwo = 5
 console.log("1.1")
 
 let sum = numOne + numTwo
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
 console.log("Sum: ", sum)
 
 console.log("---- ---- ----")
@@ -19,8 +19,8 @@ console.log("1.2")
 
 
 let difference = numOne - numTwo
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
 console.log("Difference: ", difference)
 
 console.log("---- ---- ----")
@@ -28,24 +28,24 @@ console.log("---- ---- ----")
 console.log("1.3")
 
 
-let product= numOne * numTwo
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
+let product = numOne * numTwo
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
 console.log("Product: ", product)
 
 console.log("---- ---- ----")
 
 console.log("1.4")
-let division= numOne / numTwo
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
+let division = numOne / numTwo
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
 console.log("Division: ", division)
 
 
 console.log("1.5")
-let remainder= numOne % numTwo
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
+let remainder = numOne % numTwo
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
 console.log("Remainder: ", remainder)
 
 console.log("---- ---- ----")
@@ -57,10 +57,10 @@ console.log("---- ---- ----")
 let numThree = 25
 console.log("2.1")
 
-let Sum = numOne + numTwo+numThree
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
-console.log("Number Three:" , numThree)
+let Sum = numOne + numTwo + numThree
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
+console.log("Number Three:", numThree)
 console.log("Sum: ", Sum)
 
 
@@ -68,37 +68,37 @@ console.log("---- ---- ----")
 
 console.log("2.2")
 
-let addAndSubstract= numThree - (numOne + numTwo)
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
-console.log("Number Three:" , numThree)
+let addAndSubstract = numThree - (numOne + numTwo)
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
+console.log("Number Three:", numThree)
 console.log(": Result", addAndSubstract)
 
 console.log("---- ---- ----")
 
 console.log("2.3")
 
-let productAndAdd = (numOne * numTwo)+numThree
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
-console.log("Number Three:" , numThree)
-console.log("Result: ",productAndAdd)
+let productAndAdd = (numOne * numTwo) + numThree
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
+console.log("Number Three:", numThree)
+console.log("Result: ", productAndAdd)
 
 console.log("---- ---- ----")
 
 console.log("2.4")
 
-let divideAndAdd = (numThree /numOne)+ numTwo
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
+let divideAndAdd = (numThree / numOne) + numTwo
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
 console.log("Result: ", divideAndAdd)
 
 console.log("---- ---- ----")
 console.log("2.5")
 
-let average = (numOne + numTwo+numThree)/3
-console.log("Number One:" , numOne )
-console.log("Number Two:" , numTwo)
+let average = (numOne + numTwo + numThree) / 3
+console.log("Number One:", numOne)
+console.log("Number Two:", numTwo)
 console.log("Average: ", average)
 
 

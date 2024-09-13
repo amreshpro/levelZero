609965d80f13e5d1698633c020500200a7ade71c
amreshpro <amreshprof999@gmail.com>
2024-09-13 08:18:30 +0530

    start A3

---
+++ A3.1_HW_1.js
@@ -0,0 +1,12 @@
+console.log("A3.1_HW_1");
+console.log("---- ---- ----");
+
+console.log("1.1");
+let age = 18
+let isAdult = true
+
+if(isAdult){
+    console.log("Adult");
+}
+
+console.log("Age: ",age);
